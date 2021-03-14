964f5776b59dcf21773136094f4e9328877a08e1
Sergey Ivanov <imports@mail.ru>
2021-03-14 20:41:31 +0300

    Minor fixes

--- main.py
+++ main.py
@@ -1,88 +1,49 @@
 import numpy as np
+from numpy.linalg import norm
+
+I = np.eye(3)
 
 
-def skew(vector):
+def Skew(vector):
     return np.array([[0, -vector[2], vector[1]],
                      [vector[2], 0, -vector[0]],
                      [-vector[1], vector[0], 0]])
 
 
-I = np.eye(3)
-
-
-# Theta - rotation distance, W - axis of rotation
-def RodriguesFormula(theta, w):
-    anw = I + np.sin(theta) * skew(w) + (1 - np.cos(theta)) * np.dot(skew(w), skew(w))
-    return anw
-
-
-res = RodriguesFormula(np.pi, [0, 0, 1])
-print(f"Res: \n {res} \n")
-
-
-def TranslationPoE(theta, s_v):
-    anw = np.vstack([I, [0, 0, 0]])
-
-    last_column = [[s_v * theta], [s_v * theta], [s_v * theta], [1]]
-    anw = np.hstack([anw, last_column])
-
-    return anw
-
-
-def RotationPoE(theta, s_v):
-    rodr = RodriguesFormula(theta, s_v)
-
-    anw = np.vstack([rodr, [0, 0, 0]])
-
-    rodr_dash = I * theta + (1 - np.cos(theta)) * skew()
-    #last_column =
-    anw = np.hstack([anw, [[s_v * theta], [s_v * theta], [s_v * theta], [1]]])
-
-    return anw
-
-
-from numpy import array,mat,sin,cos,dot,eye
-from numpy.linalg import norm
-
-def rodrigues(r):
-    def S(n):
-        Sn = array([[0,-n[2],n[1]],[n[2],0,-n[0]],[-n[1],n[0],0]])
-        return Sn
-    theta = norm(r)
-    if theta > 1e-30:
-        n = r/theta
-        Sn = S(n)
-        R = eye(3) + sin(theta)*Sn + (1-cos(theta))*dot(Sn,Sn)
-    else:
-        Sr = S(r)
-        theta2 = theta**2
-        R = eye(3) + (1-theta2/6.)*Sr + (.5-theta2/24.)*dot(Sr,Sr)
-    return mat(R)
-
-def so3ToVec(so3mat):
-    return np.array([so3mat[2][1], so3mat[0][2], so3mat[1][0]])
-
 def NearZero(z):
     return abs(z) < 1e-6
+
+
+def SkewToVec(skew):
+    return np.array([skew[2][1], skew[0][2], skew[1][0]])
+
+
+def VecTose3(V):
+    return np.r_[np.c_[Skew([V[0], V[1], V[2]]), [V[3], V[4], V[5]]], np.zeros((1, 4))]
+
+
 def Normalize(V):
     return V / np.linalg.norm(V)
+
 
 def AxisAng3(expc3):
     return (Normalize(expc3), np.linalg.norm(expc3))
 
+
 def MatrixExp3(so3mat):
-    omgtheta = so3ToVec(so3mat)
+    omgtheta = SkewToVec(so3mat)
+
     if NearZero(np.linalg.norm(omgtheta)):
         return np.eye(3)
     else:
         theta = AxisAng3(omgtheta)[1]
         omgmat = so3mat / theta
-        return np.eye(3) + np.sin(theta) * omgmat \
-               + (1 - np.cos(theta)) * np.dot(omgmat, omgmat)
+        return np.eye(3) + np.sin(theta) * omgmat + (1 - np.cos(theta)) * np.dot(omgmat, omgmat)
+
 
 def MatrixExp6(se3mat):
-    se3mat = np.array(se3mat)
-    omgtheta = so3ToVec(se3mat[0: 3, 0: 3])
+    omgtheta = SkewToVec(se3mat[0: 3, 0: 3])
+
     if NearZero(np.linalg.norm(omgtheta)):
         return np.r_[np.c_[np.eye(3), se3mat[0: 3, 3]], [[0, 0, 0, 1]]]
     else:
@@ -96,54 +57,35 @@ def MatrixExp6(se3mat):
                                   se3mat[0: 3, 3]) / theta],
                      [[0, 0, 0, 1]]]
 
-def VecToso3(omg):
-    return np.array([[0,      -omg[2],  omg[1]],
-                     [omg[2],       0, -omg[0]],
-                     [-omg[1], omg[0],       0]])
 
-def VecTose3(V):
-    return np.r_[np.c_[VecToso3([V[0], V[1], V[2]]), [V[3], V[4], V[5]]],
-                 np.zeros((1, 4))]
-
-def FKinBody(M, Blist, thetalist):
-    T = np.array(M)
-    for i in range(len(thetalist)):
-        T = np.dot(T, MatrixExp6(VecTose3(np.array(Blist)[:, i] \
-                                          * thetalist[i])))
-    return T
-
-L2 = 10
-L1 = 10
+q = np.array([-np.pi/2, 0, 10])
+links_length = [10, 10]
 
 M = np.array([[1, 0, 0, 0],
-              [0, 1, 0, L2],
-              [0, 0, 1, L1],
+              [0, 1, 0, links_length[1]],
+              [0, 0, 1, links_length[0]],
               [0, 0, 0, 1]])
+
 Slist = np.array([[0, 0, 1, 0, 0, 0],
                   [1, 0, 0, 0, 1, 0],
                   [0, 0, 0, 0, 1, 0]]).T
-thetalist = np.array([-np.pi/2, 0, 10])
 
-# print(FKinBody(M, Blist, thetalist))
 
 def FKinSpace(M, Slist, thetalist):
     T = np.array(M)
+
+    # Reverse loop
     for i in range(len(thetalist) - 1, -1, -1):
-        T = np.dot(MatrixExp6(VecTose3(np.array(Slist)[:, i] \
-                                       * thetalist[i])), T)
+
+        se3 = VecTose3(np.array(Slist)[:, i] * thetalist[i])
+
+        # exp ^ (S * theta)
+        exp6 = MatrixExp6(se3)
+
+        T = np.dot(exp6, T)
+
     return T
 
-# T = np.linalg.multi_dot([Rz(-np.pi/2),
-#                              Tz(10),
-#                              Rx(0),
-#                              Ty(10),
-#                              Ty(10)])
 
-print(FKinSpace(M, Slist, thetalist))
-#print(T)
-
-def Adjoint(T):
-    R = T[0: 3, 0: 3]
-    p = T[0: 3, 3]
-    return np.r_[np.c_[R, np.zeros((3, 3))],
-                 np.c_[np.dot(VecToso3(p), R), R]]
+anw = FKinSpace(M, Slist, q)
+print(anw)
