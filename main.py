9bb8715603e02af5fd7494eae37e5dce5da0a60c
AndronovaAA <43322190+AndronovaAA@users.noreply.github.com>
2021-03-15 17:49:41 +0300

    added spatial Jacobian

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import numpy as np
+from MatrixHelper import *
 from numpy.linalg import norm
+import numpy as np
 
 I = np.eye(3)
-
 
 def Skew(vector):
     return np.array([[0, -vector[2], vector[1]],
@@ -42,6 +42,7 @@ def MatrixExp3(so3mat):
 
 
 def MatrixExp6(se3mat):
+
     omgtheta = SkewToVec(se3mat[0: 3, 0: 3])
 
     if NearZero(np.linalg.norm(omgtheta)):
@@ -53,12 +54,12 @@ def MatrixExp6(se3mat):
                            np.dot(np.eye(3) * theta \
                                   + (1 - np.cos(theta)) * omgmat \
                                   + (theta - np.sin(theta)) \
-                                    * np.dot(omgmat,omgmat),
+                                    * np.dot(omgmat, omgmat),
                                   se3mat[0: 3, 3]) / theta],
                      [[0, 0, 0, 1]]]
 
 
-q = np.array([-np.pi/2, 0, 10])
+thetalist = np.array([-np.pi/2, 0, 10])
 links_length = [10, 10]
 
 M = np.array([[1, 0, 0, 0],
@@ -87,5 +88,31 @@ def FKinSpace(M, Slist, thetalist):
     return T
 
 
-anw = FKinSpace(M, Slist, q)
+anw = FKinSpace(M, Slist, thetalist)
 print(anw)
+
+T = np.linalg.multi_dot([Rz(thetalist[0]),
+                             Tz(10),
+                             Rx(thetalist[1]),
+                             Ty(10),
+                             Ty(thetalist[2])])
+
+# print(FKinSpace(M, Slist, thetalist))
+# print(T)
+
+def Adjoint(T):
+    R = T[0: 3, 0: 3]
+    p = T[0: 3, 3]
+    return np.r_[np.c_[np.transpose(R), np.dot(np.transpose(R), Skew(p))],
+                 np.c_[np.zeros((3, 3)), np.transpose(R)]]
+
+def JacobianSpace(Slist, thetalist):
+    Js = np.array(Slist).copy().astype(float)
+    T_J = np.eye(4)
+    for i in range(1, len(thetalist)):
+        T_J = np.dot(T_J, MatrixExp6(VecTose3(np.array(Slist)[:, i - 1] * thetalist[i - 1])))
+        Js[:, i] = np.dot(Adjoint(T_J), np.array(Slist)[:, i])
+    return Js
+
+print("Jacobian spatial:")
+print(JacobianSpace(Slist, thetalist))
